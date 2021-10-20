53b755282b192a48e1f9dd695058ee3541d21672
Bashar <bashar.khan.17.1@mountblue.tech>
2021-10-20 20:06:49 +0530

    Create new checkitem

--- src/components/CheckItem.jsx
+++ src/components/CheckItem.jsx
@@ -7,10 +7,31 @@ class CheckItem extends Component {
     super(props);
     this.state = {
       checkItems: [],
+      name:""
     };
   }
 
-  // This section is for fetching checklists
+  // This section is for creating new checkitems
+
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const resp = await TrelloApi.addCheckItem(this.props.checkListId, this.state.name);
+    console.log("response",resp);
+
+    this.setState({
+      checkItems: [resp, ...this.state.checkItems],
+    });
+  };
+
+  
+  
+  // This section is for fetching checkitems
 
   fetchCheckItems = async (checkListId) => {
     const checkItems = await TrelloApi.getCheckItem(checkListId);
@@ -29,6 +50,23 @@ class CheckItem extends Component {
         {this.state.checkItems.map((item) => (
           <p>{item.name}</p>
         ))}
+        <div>
+        <form onSubmit={this.handleSubmit}>
+            <div className="mb-3">
+              <input
+                value={this.state.name}
+                type="text"
+                className="form-control"
+                id="board-name"
+                onChange={this.handleChange}
+                placeholder="Checkitem Name here"
+              />
+            </div>
+            <button type="submit" className="btn btn-primary btn-sm m-2">
+              Add CheckItem
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
