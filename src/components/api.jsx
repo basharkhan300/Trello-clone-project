3be99a6fd359834f43090ac0c4b3b2b62cad9531
Bashar <bashar.khan.17.1@mountblue.tech>
2021-10-20 16:33:19 +0530

    Add fetch checkitem api

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_AUTH_KEY;
 const API_TOKEN = process.env.REACT_APP_AUTH_TOKEN;
-// console.log(API_KEY)
 
 export function getBoards() {
   return axios
@@ -36,52 +35,75 @@ export function createList(name, idBoard) {
     .then((res) => res.data);
 }
 
-export function archiveList(listId){
-
+export function archiveList(listId) {
   return axios
-  .put(
-    `https://api.trello.com/1/lists/${listId}/closed?key=${API_KEY}&token=${API_TOKEN}&value=true`
-
-  )
-  .then((res) => res.data)
-  
+    .put(
+      `https://api.trello.com/1/lists/${listId}/closed?key=${API_KEY}&token=${API_TOKEN}&value=true`
+    )
+    .then((res) => res.data);
 }
 
-export function getCards(cardId){
-
+export function getCards(cardId) {
   return axios
-  .get(
-    `https://api.trello.com/1/lists/${cardId}/cards?&key=${API_KEY}&token=${API_TOKEN}`
-    // `https://api.trello.com/1/lists/${cardId}/cards?key=${API_KEY}&token=${API_TOKEN}`
-  )
-  .then((res) => res.data)
-  
+    .get(
+      `https://api.trello.com/1/lists/${cardId}/cards?&key=${API_KEY}&token=${API_TOKEN}`
+    )
+    .then((res) => res.data);
 }
 
-export function createCards(idList, name){
-  console.log("In card" + idList);
+export function createCards(idList, name) {
   return axios
-  .post(
-    `https://api.trello.com/1/cards?idList=${idList}&name=${name}&key=${API_KEY}&token=${API_TOKEN}`
-  )
-  .then((res) => res.data)
-  
+    .post(
+      `https://api.trello.com/1/cards?idList=${idList}&name=${name}&key=${API_KEY}&token=${API_TOKEN}`
+    )
+    .then((res) => res.data);
 }
 
-// export function deleteCard(idCard){
-
-//   return axios
-//   .delete(
-//     `https://api.trello.com/1/cards/${idCard}&key=${API_KEY}&token=${API_TOKEN}`
-//   )
-  
-// }
-
-export function getOneCard(cardId){
-console.log("cardid in apijsx is "+ cardId );
+export function deleteCard(idCard) {
   return axios
-  .get(
-    `https://api.trello.com/1/cards/${cardId}?&key=${API_KEY}&token=${API_TOKEN}`
+    .delete(
+      `https://api.trello.com/1/cards/${idCard}?key=${API_KEY}&token=${API_TOKEN}`
+    )
+    .then((res) => res.data);
+}
+
+export function getOneCard(cardId) {
+  return axios
+    .get(
+      `https://api.trello.com/1/cards/${cardId}?&key=${API_KEY}&token=${API_TOKEN}`
+    )
+    .then((res) => res.data);
+}
+
+export function getChecklist(cardId) {
+  return axios
+    .get(
+      `https://api.trello.com/1/cards/${cardId}/checklists?key=${API_KEY}&token=${API_TOKEN}`
+    )
+    .then((res) => res.data);
+}
+
+export function addChecklist(cardId, name) {
+  return axios
+    .post(
+      `https://api.trello.com/1/cards/${cardId}/checklists?&name=${name}&key=${API_KEY}&token=${API_TOKEN}`
+    )
+    .then((res) => res.data);
+}
+
+export function deleteCheckList(cardId) {
+  return axios
+    .delete(
+      `https://api.trello.com/1/checklists/${cardId}?key=${API_KEY}&token=${API_TOKEN}`
+    )
+    .then((res) => res.data);
+}
+
+
+export function getCheckItem(checkListId){
+
+  return axios.get(
+    `https://api.trello.com/1/checklists/${checkListId}/checkItems?key=${API_KEY}&token=${API_TOKEN}`
   )
   .then((res) => res.data)
   
